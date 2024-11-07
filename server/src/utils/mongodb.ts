a595de1146aaa110bc93b4cdd9cb96fd960520d0
kaushaltheeG <kaushalsk09@gmail.com>
2024-11-07 02:21:41 -0500

    debug env

--- server/src/utils/mongodb.ts
+++ server/src/utils/mongodb.ts
@@ -6,15 +6,15 @@ dotenv.config()
 const uri = process.env.MONGO_URI || '';
 const dbName = process.env.MONGO_DB_NAME;
 
-if (!uri) {
-  throw new Error("MONGO_URI is not set");
-}
 
 let connectedDatabase: Db | null = null;
 
 // Connect to the mongo database
 export const connectDB = async (): Promise<Db> => {
   try {
+    if (!uri) {
+      throw new Error("MONGO_URI is not set");
+    }
     const client = new MongoClient(uri, {
       serverApi: {
         version: ServerApiVersion.v1,
@@ -27,6 +27,7 @@ export const connectDB = async (): Promise<Db> => {
     console.log('MongoDB Connected...');
     return connectedDatabase;
   } catch (err) {
+    console.error(`MONGO URI: ${uri}`)
     console.error('Failed to connect to MongoDB:', err);
     process.exit(1);
   }
